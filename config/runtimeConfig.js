2725565c487e703eaef10cee20232138bdc84961
Jeff Goldberg <goldbergjeffrey@yahoo.com>
2016-12-05 12:27:29 -0500

    finalizing plugin

--- config/runtimeConfig.js
+++ config/runtimeConfig.js
@@ -1,12 +1,26 @@
 var extend = require('extend');
 var mainConfig = require("../../../config/config");
-var testConfig = require('./testConfig');
+var baseConfig = require('./baseConfig');
 
 
 var config;
 
 
 config = extend(true, mainConfig, config);
-config = extend(true, testConfig, config);
-
+config = extend(true, baseConfig, config);
+config = extend(true, config, {
+    qsocks: {
+        ca: fs.readFileSync(path.resolve(config.certPath,'root.pem')),
+        key: fs.readFileSync(path.resolve(config.certPath,'client_key.pem')),
+        cert: fs.readFileSync(path.resolve(config.certPath,'client.pem')),
+        headers: {
+            "X-Qlik-User" : config.qsocks.repoAccount
+        }
+    },
+    qrs: {
+		localCertPath: certPath,
+		repoAccountUserDirectory: 'INTERNAL',
+		repoAccountUserId: 'sa_repository'
+	}
+});
 module.exports = config;
