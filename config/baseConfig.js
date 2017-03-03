9f796033f8b6bce22f0e6bd927913467be7fcf4a
Jeffrey Goldberg <goldbergjeffrey@yahoo.com>
2017-03-03 13:15:24 -0500

    qrs calls now get app list based on authenticated user.

--- config/baseConfig.js
+++ config/baseConfig.js
@@ -2,10 +2,10 @@ var extend = require('extend');
 var path = require('path');
 
 var config = {
-	qsocks: {
-		repoAccount: 'UserDirectory=Internal;UserId=sa_repository'
-	},
-	outputPath: path.resolve(__dirname,"..","output")
+    qsocks: {
+        repoAccount: 'UserDirectory=Internal;UserId=sa_api'
+    },
+    outputPath: path.resolve(__dirname, "..", "output")
 };
 
 module.exports = config;
